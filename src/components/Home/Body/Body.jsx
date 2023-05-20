d3f8336827b2a190c28f9a3f72f3524cb658c4ad
Utsho11 <utshoroy521@gmail.com>
2023-05-20 20:15:04 +0600

    gallery added

--- src/components/Home/Body/Body.jsx
+++ src/components/Home/Body/Body.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Category from './Category/Category';
 import Banner from '../Banner/Banner';
+import Gallery from '../Gallery/Gallery';
 
 const Body = () => {
     return (
         <div>
             <Banner></Banner>
+            <Gallery></Gallery>
             <Category></Category>
         </div>
     );
---
+++ src/components/Home/Gallery/Gallery.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div className='my-8 mx-4'>
+            <h1 className='text-white text-5xl text-center font-bold my-4 first-letter: first-letter:'>GALLERY</h1>
+            <div className="carousel w-full">
+                <div id="item1" className="carousel-item flex justify-center w-full gap-4">
+                    <img style={{width: "500px",height: "500px"}} src="https://static-01.daraz.com.bd/p/fa397a4e221439143b9d0c91de6ff1ef.jpg" className="w-1/2" />
+                    <img style={{width: "500px",height: "500px"}} src="https://static-01.daraz.com.bd/p/d7c03ada749b50b8088e21dddb480fb7.jpg" className="w-1/2" />
+                </div>
+                <div id="item2" className="carousel-item flex justify-center w-full gap-4">
+                    <img style={{width: "500px",height: "500px"}} src="https://i5.walmartimages.com/asr/00b78dd8-e016-49ee-8156-48529c32a700.c8d47545b37e03936fd60d0eb06f05cf.jpeg?odnHeight=784&odnWidth=580&odnBg=FFFFFF" className="w-1/2" />
+                    <img style={{width: "500px",height: "500px"}} src="https://cdn.s7.shopdisney.eu/is/image/DisneyStoreES/461012708580-5?fmt…" className="w-1/2" />
+                </div>
+                <div id="item3" className="carousel-item flex justify-center w-full gap-4">
+                    <img style={{width: "500px",height: "500px"}} src="https://images-na.ssl-images-amazon.com/images/I/810MxkohwsL._AC_UL600_SR600,600_.jpg" className="w-full" />
+                    <img style={{width: "500px",height: "500px"}} src="https://bwtf.com/sites/default/files/toyreview-images/generations/earthrise/starscream/inbox.jpg" className="w-full" />
+                </div>
+            </div>
+            <div className="flex justify-center w-full py-2 gap-2">
+                <a href="#item1" className="btn btn-xs">1</a>
+                <a href="#item2" className="btn btn-xs">2</a>
+                <a href="#item3" className="btn btn-xs">3</a>
+            </div>
+        </div>
+    );
+};
+
+export default Gallery;
